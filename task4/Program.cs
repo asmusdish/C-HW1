021f5feb9b14aa41457f1ee9c4a173ef7b16975b
Denis <asmusdish@ya.ru>
2023-01-14 13:42:36 +0500

    task4 ready

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Выводит массив всех четных чисел от 1 до N");
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 2; i <= N; i = i + 2)
+{
+    Console.WriteLine(i);
+}
