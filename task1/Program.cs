f0227cf243bc97cc6fb15b8e30222cfdb0cba927
Denis <asmusdish@ya.ru>
2023-01-14 12:18:32 +0500

    task1 ready

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Программа сравнения двух чисел");
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+Console.WriteLine("max = " + number1);
+Console.WriteLine("min = " + number2);
+}
+else {Console.WriteLine("min = " + number1);
+     Console.WriteLine("max = " + number2);}
